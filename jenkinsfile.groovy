94c05e2b8429bea3031de84f22c9ecc7fb625451
abrarsyed12 <130308323+abrarsyed12@users.noreply.github.com>
2024-11-29 21:00:25 +0530

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,20 @@
+pipeline{
+  agent any
+  stages{
+    stage('code'){
+      steps{
+        echo "code"
+      }
+    }
+    stage('build'){
+      steps{
+        echo "build"
+      }
+    }
+    stage('push'){
+      steps{
+        echo "push"
+      }
+    }
+  }
+}
